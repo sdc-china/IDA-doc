796d20a9436e3330aa37c054d7f964cf76cea330
DI LU <ludidl@cn.ibm.com>
2023-12-19 13:31:06 +0800

    Update deploy-ida-liberty.sh

--- scripts/deploy-ida-liberty.sh
+++ scripts/deploy-ida-liberty.sh
@@ -13,27 +13,15 @@ fi
 
 #process input parameters
 zippath=$1
-filename=$(basename $zippath)
-filenamenoext="${filename%.*}"
-zipdir=$(dirname "$1")
-
-if [[ "$zippath" = /* ]];then
-  targetdir="${zipdir}/${filenamenoext}"
-else
-  targetdir="$(pwd)/$filenamenoext"
-fi
 
 libertypath=$2
 serversdir=$(dirname "$2")
 idaserver=$(basename $libertypath)
 
-if [ -d "$targetdir" ]; then
-  echo "$targetdir already exists, deleting it."
-  rm -Rf $targetdir;
-fi
+scriptdir=$(pwd)
 
-echo "Unzip $zippath to $targetdir."
-unzip -q $zippath -d $targetdir
+echo "Unzip ida-web.war in ${zippath} to script's directory."
+unzip -o -j $zippath "build/ida-web.war"
 
 echo "Goto liberty bin directory"
 cd $serversdir/../../
@@ -43,21 +31,25 @@ cd bin
 bindir=$(pwd)
 
 echo "Stop liberty server"
-./server stop ${idaserver}
+./server stop $idaserver
 
-wardir="${targetdir}/build/ida-web.war"
+warfile="${scriptdir}/ida-web.war"
 appdir="${libertydir}/usr/servers/${idaserver}/apps/"
 
 cd $appdir
 timestamp=$(date +%s)
 echo "Backup previous war file to ${appdir}ida-web.war.${timestamp}"
-mv "$appdir/ida-web.war" "$appdir/ida-web.war.$timestamp"
+mv "${appdir}ida-web.war" "${appdir}ida-web.war.${timestamp}"
 
 echo "Copy new war file to liberty"
-cp $wardir $appdir
+cp $warfile $appdir
 
 echo "Start liberty server"
 cd $bindir
 ./server start $idaserver
+
+echo "Clean previous unzipped ida-web.war"
+cd $scriptdir
+rm -Rf ida-web.war;
 
 echo "End"
